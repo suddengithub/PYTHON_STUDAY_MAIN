628eba8b3c44f17241a3f76c07612073d12239d7
suddengithub <konyu1007@gmail.com>
2024-09-13 16:33:54 +0900

    9/13 16:33

--- 240913 딕셔너리커피메뉴.py
+++ 240913 딕셔너리커피메뉴.py
@@ -1,6 +1,10 @@
 # 커피 메뉴 만들기
 # [1] 메뉴 보기 [2] 메뉴 조회 [3] 메뉴 추가 [4] 메뉴 삭제 [5] 종료하기
 # 기본 메뉴 만들기
+import json
+
+from anaconda_navigator.utils.url_utils import file_name
+
 menu = {
     "Americano": ["Coffee", 2000, "기본 커피입니다."],
     "Espresso" : ["Coffee", 2500, "진한 커피입니다."],
@@ -13,6 +17,21 @@ menu = {
     "GreenAde": ["Ade", 5000, "포도 에이드 입니다."],
     "LemonAde": ["Ade", 4500, "레몬 에이드 입니다."]\
 }
+file_name = "menu.json"
+# 파일에서 메뉴를 읽어오는 함수
+def load_menu():
+    try:
+        with open(file_name, "r", enconding="utf-8") as file:
+            return json.load(file):
+    except FileNotFoundError:
+        print("해당 파일이 없습니다.")
+    except json.JSONDecodeError:
+        print("JSON 디코딩 실패")
+
+# 파일에 저장하는 함수
+def save_menu():
+    with open(file_name, "w", encoding="utf-8") as file:
+        json.dump(menu, file, ensure_ascii=False, indent=4)
 
 # [1] 메뉴 보기
 def print_menu():
@@ -31,7 +50,7 @@ def get_menu(key):
 # [3] 메뉴 추가
 def add_menu(key, category, price, desc):   # key, 분류, 가격, 설명
     if key not in menu: # 해당 키에 대한 메뉴가 없음
-        menu[key] = [cateogry, price, desc]
+        menu[key] = [category, price, desc]
         print(f"{key} 메뉴가 추가 되었습니다.")
     else:
         print("메뉴가 이미 존재합니다.")
@@ -42,6 +61,33 @@ def del_menu(key):
         print(f"{key} 메뉴가 삭제 되었습니다.")
     else:
         print("삭제할 메뉴가 없습니다")
+
+while True:
+    print("메뉴를 선택하세요 : ")
+    sel = input("[1] 메뉴 보기 [2] 메뉴 조회 [3] 메뉴 추가 [4] 메뉴 삭제 [5] 종료하기 : ")
+    if sel == "1":
+        print_menu()
+    elif sel == "2":
+        key = input("조회할 메뉴 입력 : ")
+        get_menu(key)
+    elif sel == "3":
+        key = input("추가할 메뉴 이름 : ")
+        cate = input("분류 입력 : ")
+        price = int(input("가격 입력 : "))
+        desc = input("제품 설명 : ")
+        add_menu(key, cate, price, desc)
+    elif sel == "4":
+        key = input("삭제할 메뉴 입력 : ")
+        del_menu(key)
+    elif sel == "5":
+        print("영업을 종료합니다.")
+        break
+    else:
+        print("잘못된 입력입니다.")
+
+
+
+
 
 
 
