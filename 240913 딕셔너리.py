7e9c284ec3e6d043d6792b58864e11554c84d390
suddengithub <konyu1007@gmail.com>
2024-09-13 15:16:14 +0900

    9/13 15:16

---
+++ 240913 딕셔너리.py
@@ -0,0 +1,6 @@
+# 딕셔너리? 키로 값을 찾는 것
+# {} 중괄호로 딕셔너리 선언
+# 각 요소는 ,(쉼표)로 구분
+# 키(key)와 값(value)은 콜론(:)으로 구분
+coffee_menu = {"Americano": 2500, "Espresso": 2500, "Latte": 4000, "Moca": 4500}
+tood_menu = {"Cake":5000, "Bakery":6000}
