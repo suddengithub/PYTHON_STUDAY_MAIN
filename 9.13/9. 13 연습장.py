feb0374849fc03e4b839d9873d2469fd4753366a
suddengithub <konyu1007@gmail.com>
2024-09-13 13:33:22 +0900

    9/13 13:33

---
+++ 9.13/9. 13 연습장.py
@@ -0,0 +1,11 @@
+print(100, 200, sep="\n")
+print("010", "1234", "5678", sep="-")
+
+# 파이썬 스타일 2, 가장 최근에 추가된 방식(f-string), 3.6 이후
+# f와 {}로 사용 합니다.
+print("====== 파이썬 스타일 2 ======")
+print(f"이름 : {name}")
+print(f"나이 : {age}")
+print(f"성별 : {gender}")
+print(f"직업 : {jobs}")
+print(f"주소 : {addr}\n")
