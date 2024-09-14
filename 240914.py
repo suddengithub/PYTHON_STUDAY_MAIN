0010b218bfbcb865d295df58fa2ffa8032f60be9
H.S <konyu1007@gmail.com>
2024-09-14 23:05:52 +0900

    home 22:09

--- 240914.py
+++ 240914.py
@@ -3,3 +3,25 @@ print(name)
 
 name = "jack"
 print(name)
+
+num = 100
+print(num)
+
+# "주소 : 서울시 강남구 역삼동 KH정보교육원"
+# "전화번호 : 02-1234-5678"
+# "직책 : 강사"
+# "이름 : 정경수"
+# "이메일 : jks2024@megastudy.com"
+
+name = "정경수"
+email = "jks2024"
+position = "강사"
+addr = "서울시 강남구 역삼동 KH정보교육원"
+tel = "01-1234-5678"
+
+print(f"""
+주소 : {addr}
+전화번호 : {tel}
+직책 : {position}
+이름 : {name}
+이메일 : {email}@megastudy.com""")
